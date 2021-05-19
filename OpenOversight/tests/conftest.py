ff40d741963095e0216d6e10cdf4a5b0a6b9d2d7
abandoned-prototype <41744410+abandoned-prototype@users.noreply.github.com>
2021-05-18 22:33:48 -0500

    fix tests and formatting

--- OpenOversight/tests/conftest.py
+++ OpenOversight/tests/conftest.py
@@ -20,6 +20,7 @@ from OpenOversight.app import create_app, models
 from OpenOversight.app.utils import merge_dicts
 from OpenOversight.app.models import db as _db, Unit, Job, Officer
 from OpenOversight.app.main.choices import RACE_CHOICES
+from OpenOversight.tests.routes.route_helpers import ADMIN_EMAIL, ADMIN_PASSWORD
 
 factory = Faker()
 
@@ -340,9 +341,9 @@ def add_mockdata(session):
                             confirmed=True)
     session.add(test_user)
 
-    test_admin = models.User(email='test@example.org',
+    test_admin = models.User(email=ADMIN_EMAIL,
                              username='test_admin',
-                             password='testtest',
+                             password=ADMIN_PASSWORD,
                              confirmed=True,
                              is_administrator=True)
     session.add(test_admin)
